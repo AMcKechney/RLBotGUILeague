c2dea243accf14ac3ac367efdcda4c5b9409fe3f
Aidan McKechney <aidan.mckechney@wilberts.com>
2025-01-11 00:18:09 -0500

    Add bot filtering and custom naming in MatchStarter

--- MatchStarter/run.py
+++ MatchStarter/run.py
@@ -1,10 +1,16 @@
 from PyQt5.QtCore import QSettings
 from rlbot.setup_manager import SetupManager, RocketLeagueLauncherPreference
+import json
 
 from rlbot_gui.match_runner.match_runner import start_match_helper
 from rlbot_gui.gui import get_bots_from_directory
 
 human = {'name': 'Human', 'team': 0, 'type': 'human', 'skill': None, 'path': None}
+
+def load_accepted_bot_list(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
 
 def load_bots():
     BOT_FOLDER_SETTINGS_KEY = 'bot_folder_settings'
@@ -33,9 +39,17 @@ names = [bot['name'] for bot in bots]
 print(names)
 # pass
 
+accepted_bots = load_accepted_bot_list('MatchStarter/bots.json')['bots']
+
+bots = [bot for bot in bots if bot['name'] in accepted_bots]
+
+
 bot_list = []
-bot_1 = bots[1]
-bot_list.append({'name': bot_1['name'], 'team': 0, 'type': 'rlbot', 'skill': 1, 'path': bot_1['path']})
+bot_1 = bots[0]
+bot_list.append({'name': 'test_custom_name', 'team': 0, 'type': 'rlbot', 'skill': 1, 'path': bot_1['path']})
+
+bot_2 = bots[1]
+bot_list.append({'name': 'test_custom_name', 'team': 1, 'type': 'rlbot', 'skill': 1, 'path': bot_2['path']})
 launcher_prefs = RocketLeagueLauncherPreference(preferred_launcher='steam', use_login_tricks=True, rocket_league_exe_path=None)
 
 match_settings = {'map': 'DFHStadium', 'game_mode': 'Soccer', 'skip_replays': False, 'instant_start': False, 'enable_lockstep': False, 'match_behavior': 'Restart If Different', 'mutators': {'ball_bounciness': 'Default', 'ball_max_speed': 'Default', 'ball_size': 'Default', 'ball_type': 'Default', 'ball_weight': 'Default', 'boost_amount': 'Default', 'boost_strength': '1x', 'demolish': 'Default', 'game_speed': 'Default', 'gravity': 'Default', 'match_length': '5 Minutes', 'max_score': 'Unlimited', 'overtime': 'Unlimited', 'respawn_time': '3 Seconds', 'rumble': 'None', 'series_length': 'Unlimited'}, 'randomizeMap': False, 'enable_rendering': False, 'enable_state_setting': True, 'auto_save_replay': False, 'scripts': []}
